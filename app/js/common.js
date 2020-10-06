0567d96fc046e8205854e366497e4eae4191cd8a
andreyi96 <ya.andreyi96@yandex.ru>
2020-10-06 23:29:35 +0300

    Главная страница 1 блок

--- app/js/common.js
+++ app/js/common.js
@@ -2,5 +2,11 @@ $(function() {
     $('.openSearch').on('click', function () {
         $('.search').toggleClass('active')
     });
-
+    $('.js--main-banner').slick({
+        autoplay: true,
+        prevArrow: '',
+        nextArrow: '',
+        dots: true,
+        dotsClass: 'main-banner--dots'
+    });
 });
