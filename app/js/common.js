0fd9285253b1e00a04f1c42a7ac73e9d562cb33c
asemenova <alenkina127@gmail.com>
2020-10-08 00:12:39 +0300

    Каталог

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,9 @@
 $(function() {
-    $('.openSearch').on('click', function () {
-        $('.search').toggleClass('active')
+    $('.mobile_touch .search a').on('click', function () {
+        $('.mobile_touch .search').toggleClass('active')
+    });
+    $('.mobile_touch .search .close').on('click', function () {
+        $('.mobile_touch .search').toggleClass('active')
     });
     $('.burger').on('click',function () {
         $('.mobile_menu').toggleClass('active');
@@ -8,6 +11,14 @@ $(function() {
     $('.mobile_menu .close').on('click',function () {
         $('.mobile_menu').toggleClass('active');
     });
+    $('.filter_button').on('click', function () {
+        $(this).toggleClass('active');
+        $('.filter form').slideToggle();
+    });
+    // $('.catalog_products').masonry({
+    //     columnWidth: 200,
+    //     itemSelector: '.grid-item'
+    // });
     $('.js--main-banner').slick({
         autoplay: true,
         prevArrow: '',
