acd2355cee58e49b1a58da32ba01e49a8bd48760
asemenova <alenkina127@gmail.com>
2020-10-07 21:16:07 +0300

    Футер и крошки

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,12 @@ $(function() {
     $('.openSearch').on('click', function () {
         $('.search').toggleClass('active')
     });
+    $('.burger').on('click',function () {
+        $('.mobile_menu').toggleClass('active');
+    });
+    $('.mobile_menu .close').on('click',function () {
+        $('.mobile_menu').toggleClass('active');
+    });
     $('.js--main-banner').slick({
         autoplay: true,
         prevArrow: '',
